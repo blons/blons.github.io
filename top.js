d102bce0cb86c4e50e9efbf105b829d80b5caa77
blons <88247621+blons@users.noreply.github.com>
2022-05-11 20:13:39 +0200

    Update 3

---
+++ top.js
@@ -0,0 +1,33 @@
+const scrollbutton = document.getElementById('scrollbutton')
+let oldScroll = 0;
+let lock = false;
+
+function goTop() {
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+window.onscroll = function() {scrollState()}
+
+function scrollState() {
+    let sc = window.scrollY;
+    if (sc > 300 && sc < oldScroll && lock == false) {
+        // scrollbutton.style.display = 'block';
+        scrollbutton.classList.add('popup');
+        scrollbutton.classList.remove('popdown');
+        setTimeout(function(){scrollbutton.style.display = 'block';}, 50);
+        lock = true;
+        setTimeout(function(){lock = false;}, 100);
+    } else if (lock == false){
+        // scrollbutton.style.display = 'none';
+        scrollbutton.classList.add('popdown');
+        scrollbutton.classList.remove('popup');
+        setTimeout(function(){scrollbutton.style.display = 'none';}, 100);
+        lock = true;
+        setTimeout(function(){lock = false;}, 150);
+    }
+    oldScroll = sc;
+}
